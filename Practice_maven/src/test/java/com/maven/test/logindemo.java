7014c24918bbe3e02e0ff495c9a442f12da72a1b
HinalBhuva22 <radhekrushnenterprise8@gmail.com>
2024-09-30 12:40:05 +0530

    Third Commit

---
+++ Practice_maven/src/test/java/com/maven/test/logindemo.java
@@ -0,0 +1,38 @@
+package com.maven.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class logindemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.fb.com");
+		driver.manage().window().maximize();
+		
+		WebElement email = driver.findElement(By.id("email"));
+		WebElement pass = driver.findElement(By.id("pass"));
+		WebElement login = driver.findElement(By.name("login"));
+		
+		
+		email.sendKeys("test@gmail.com");
+		pass.sendKeys("pass123");
+		login.click();
+		
+		
+				
+
+	}
+
+
+	}
+
+
