059a1d2399b6eaabc55098a997bdbba20b9a9f91
HinalBhuva22 <radhekrushnenterprise8@gmail.com>
2024-09-30 11:43:36 +0530

    First Commit

---
+++ Practice_maven/src/test/java/com/maven/test/openbrowser.java
@@ -0,0 +1,19 @@
+package com.maven.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class openbrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
